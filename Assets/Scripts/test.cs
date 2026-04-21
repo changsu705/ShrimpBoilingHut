9d353f44d9e397a10fb529ec9e6551bae988682e
changsu705 <2021137031@g.shingu.ac.kr>
2026-04-21 12:12:30 +0900

    [Feat] test 추가

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // This is a simple test script that logs a message to the console when the game starts.
+        // You can replace this with any initialization code you need.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
