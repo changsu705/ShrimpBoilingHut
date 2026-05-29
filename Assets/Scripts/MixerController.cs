bba7ee6d00356db71393b5429916c9f4a3d543b3
juhog282 <juhog282@g.shingu.ac.kr>
2026-05-29 20:34:01 +0900

    5/29 Singleton

--- Assets/Scripts/MixerController.cs
+++ Assets/Scripts/MixerController.cs
@@ -4,6 +4,9 @@ using UnityEngine.UI;
 
 public class MixerController : MonoBehaviour
 {
+    // [추가] 어디서나 이 믹서 컨트롤러에 접근할 수 있도록 static 인스턴스 선언
+    public static MixerController instance { get; private set; }
+
     [Header("[오디오 믹서 지정]")]
     [SerializeField] private AudioMixer m_AudioMixer;
 
@@ -14,6 +17,18 @@ public class MixerController : MonoBehaviour
 
     private void Awake()
     {
+        //  싱글톤 검사 및 방어 코드
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // 씬이 바뀌어도 파괴되지 않고 유지
+        }
+        else
+        {
+            Destroy(gameObject); // 이미 존재한다면 새로 생겨난 중복 매니저 파괴
+            return; // 아래 리스너 등록 코드가 실행되지 않도록 즉시 종료
+        }
+
         // 슬라이더 값이 변경될 때마다 각각의 볼륨 조절 함수를 실행하도록 연결
         m_MusicMasterSlider.onValueChanged.AddListener(SetMasterVolume);
         m_MusicBGMSlider.onValueChanged.AddListener(SetMusicVolume);
